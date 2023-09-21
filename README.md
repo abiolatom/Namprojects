ac846d8ef8187166d76134a2c8920cc8a1bb4945
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-09-21 19:07:33 +0300

    second day commit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Namprojects
 
 In this project, I learnt react by building a large project following up an experienced developoer's footsteps. I have realised this ios one of the fastest way for me to learn based on my recent education. Morever, since I also have a lot to learn in a short period. a learning curve from tough/large to medium and small is the best way to achieve my goals within the shortest possible period. I welcome you to this ride.
+
+DAY 1 and 2:
+
+Learning about starting react app and npm:
+--- npm init
+--- npm Dev and Normal Dependencies
+--- krep and tilde
+--- transitive dependencies: Parcel the beast and others
+--- Node modules
+-- npm: Node modules
+-- npx: parcel execution
+
+---Component composition: Nesting components inside one another
--- app.js
+++ app.js
@@ -1,19 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, 
-React.createElement("div", {id:"child"}, [
-React.createElement("h1", {}, "I am an h1 tag"),
-React.createElement("h2", {}, "I am an h2 tag")
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src="https://downtownmidland.com/wp-content/uploads/2020/12/A1-900x600.jpg" />
+            </div>
+            <div className="nav-items" >
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
 
-]));
+            </ul>
+            </div>
+        </div>
+    );
+};
 
-console.log(parent);
+const ResCard = () => {
+    return (
+        <div className="res-card" style={{backgroundColor:"#f0f0f0" }}>
+            <div className="res-card-img">
+                <img className="res-logo" alt="res-logo" src="https://cdn.stocksnap.io/img-thumbs/960w/pizza-wine_IJESKJTYB6.jpg" />
+                <h3>Meghan Foods</h3>
+                <h4>Pizza, Sumose, Cutle</h4>
+                <h4>4.5 stars</h4>
+                <h4>10 mins</h4>
+            </div>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search"> Search</div>
+            <div className="res-card-container">
+                <ResCard />
+                <ResCard />
+                <ResCard />
+                <ResCard />
+                <ResCard />
+                <ResCard />
+                <ResCard />
+            </div>
+        </div>
+    );
+};
 
 
-
-/* const heading = React.createElement(
-    "h1", {id:"heading"}, "Hello World from React!"); */
+const AppLayout = () => {
+    return (
+        <div className ="app">
+           <Header/>
+           <Body/>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
 
