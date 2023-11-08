e397f20b9af7a92dd3c01741cfc77567db99607a
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-11-08 19:23:55 +0200

    api call

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,22 +1,32 @@
 import resList from "../utils/mockData";
 import ResCard from "./ResCard";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
   const handleTopRatedRestaurantsClick = () => {
-    const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+    const filteredList = listOfRestaurants.filter(
+      (res) => res.info.avgRating > 4
+    );
     setListOfRestaurants(filteredList);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+  const fetchData = async (req, res) => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
   };
 
   return (
     <div className="body">
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={handleTopRatedRestaurantsClick}
-        >
+        <button className="filter-btn" onClick={handleTopRatedRestaurantsClick}>
           Top Rated Restaurants
         </button>
       </div>
